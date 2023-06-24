8b789b29d7d3de1bf8432ed24e1bfe6405bf37bf
RomanMatkovskyi <roman.matkovskyii@gmail.com>
2023-06-24 14:52:22 +0200

    select style

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,9 +1,12 @@
 import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 let apiKey;
 const selectEl = document.querySelector('.breed-select');
 const errorEl = document.querySelector('.error');
 const ploadEl = document.querySelector('.load');
+const titleEl = document.querySelector('.main-title');
 
 const formEl = document.getElementById('api');
 const formInput = document.querySelector('.api-input');
@@ -13,50 +16,62 @@ const backDropEl = document.querySelector('.backdrop');
 
 const divCatInfoEl = document.querySelector('.cat-info');
 errorEl.style.display = 'none';
-window.addEventListener('load', checkApiKey)
+
+window.addEventListener('load', checkApiKey);
 function checkApiKey() {
-if(localStorage.getItem("apiKey") !== null) {
-  apiKey = localStorage.getItem("apiKey")
-  backDropEl.classList.toggle('visually-hidden')
-}
+  selectEl.classList.add('visually-hidden');
+  titleEl.classList.add('visually-hidden');
+  if (localStorage.getItem('apiKey') !== null) {
+    apiKey = localStorage.getItem('apiKey');
+    backDropEl.classList.toggle('visually-hidden');
+    selectEl.classList.remove('visually-hidden');
+    titleEl.classList.remove('visually-hidden');
+  }
 }
 
-formEl.addEventListener('submit', getApiKey)
+formEl.addEventListener('submit', getApiKey);
 
 function getApiKey(event) {
-  event.preventDefault()
+  event.preventDefault();
   apiKey = formInput.value;
   formInput.value = '';
-loaderFormEl.classList.toggle('visually-hidden');
+  loaderFormEl.classList.toggle('visually-hidden');
   fetch(`https://api.thecatapi.com/v1/images/search?limit=15`, {
     headers: {
       'x-api-key': apiKey,
-    }})
+    },
+  })
     .then(response => response.json())
-    .then (data => {
+    .then(data => {
       loaderFormEl.classList.toggle('visually-hidden');
       if (data.length === 15) {
-        localStorage.setItem("apiKey", apiKey);
-        backDropEl.classList.toggle('visually-hidden')
+        localStorage.setItem('apiKey', apiKey);
+        backDropEl.classList.toggle('visually-hidden');
       } else {
-      loaderFormEl.classList.add('visually-hidden');
-      Notiflix.Notify.failure('Upppsss, your key is wrong :(')
+        loaderFormEl.classList.add('visually-hidden');
+        Notiflix.Notify.failure('Upppsss, your key is wrong :(');
       }
+      selectEl.classList.remove('visually-hidden');
+      titleEl.classList.remove('visually-hidden');
     })
-    .catch(error => Notiflix.Notify.failure('Please enter valid key'))
+    .catch(error => Notiflix.Notify.failure('Please enter valid key'));
 }
 
-
 function fetchBreeds() {
+  ploadEl.classList.toggle('loader');
   fetch('https://api.thecatapi.com/v1/breeds')
     .then(response => response.json())
     .then(data => {
+      ploadEl.classList.toggle('loader');
       const breedListId = data.map(({ id, name }) => ({ id, name }));
       breedListId.forEach(id => {
         const optionEl = document.createElement('option');
         optionEl.value = id.id;
         optionEl.textContent = id.name;
         selectEl.appendChild(optionEl);
+      });
+      new SlimSelect({
+        select: '#single',
       });
     })
     .catch(error => (errorEl.style.display = 'block'));
@@ -75,20 +90,21 @@ function fetchCatByBreed(breedId) {
     headers: {
       'x-api-key': apiKey,
     },
-  }).then(data => data.json())
-  .then(id => {
-    ploadEl.classList.toggle('loader');
-    let catInfo = id[0];
-    let breedInfo = catInfo.breeds[0]
-    let catDescription = `
+  })
+    .then(data => data.json())
+    .then(id => {
+      ploadEl.classList.toggle('loader');
+      let catInfo = id[0];
+      let breedInfo = catInfo.breeds[0];
+      let catDescription = `
     <img src="${catInfo.url}" alt="" height="300" class="cat-foto">
     <div class="descrition-wrap">
       <h2 class="cat-name">${breedInfo.name}</h2>
       <p class="cat-description">${breedInfo.description}</p>
       <p class="cat-temperament"><span class="tempword-wrap">Temperament</span>: ${breedInfo.temperament}</p>
-      </div>`
-      divCatInfoEl.insertAdjacentHTML("beforeend", catDescription) 
-  });
+      </div>`;
+      divCatInfoEl.insertAdjacentHTML('beforeend', catDescription);
+    });
 }
 
 fetchBreeds();
